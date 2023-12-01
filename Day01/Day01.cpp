4b0f0c784342a309a786626697c767fb1f5de30f
Michael Braedley <mike.braedley@gmail.com>
2023-12-01 08:07:39 -0400

    Day 1 part 1

--- Day01/Day01.cpp
+++ Day01/Day01.cpp
@@ -4,7 +4,22 @@
 #include "Day01.h"
 #include "Utils.h"
 
+#include <cassert>
+
 int main()
 {
-	return 0;
+	auto input = utils::ReadInput("input.txt");
+
+	std::int64_t sum = 0;
+	for (auto line : input)
+	{
+		char d1 = line[line.find_first_of("0123456789")];
+		char d2 = line[line.find_last_of("0123456789")];
+
+		int v1 = (d1 - '0') * 10;
+		int v2 = (d2 - '0');
+		sum += v1 + v2;
+	}
+
+	std::cout << sum;
 }

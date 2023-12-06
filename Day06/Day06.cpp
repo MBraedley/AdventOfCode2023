d23a3528adf0b45aa87779e9a258a05eb73c419f
Michael Braedley <michael.braedley@cartenav.com>
2023-12-06 11:42:07 -0400

    Day 6 part 2

--- Day06/Day06.cpp
+++ Day06/Day06.cpp
@@ -39,5 +39,20 @@ int main()
 
 	std::cout << product << "\n";
 
+	//part 2
+	//brute forcing isn't really an option here, have to be smart about it
+	std::string init;
+	std::size_t realTime = std::stoull( std::accumulate( std::next( times.begin() ), times.end(), init ) );
+	std::size_t realDist = std::stoull( std::accumulate( std::next( dists.begin() ), dists.end(), init ) );
+
+	std::size_t t = 0;
+
+	while ( (realTime - t) * t < realDist )
+	{
+		t++;
+	}
+
+	std::cout << realTime + 1 - t * 2 << "\n";
+
 	return 0;
 }

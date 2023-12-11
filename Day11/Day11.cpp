d1529ebd932711fca54d1577d5355e762fe1f25e
Michael Braedley <michael.braedley@cartenav.com>
2023-12-11 10:00:13 -0400

    Day 11 part 2

--- Day11/Day11.cpp
+++ Day11/Day11.cpp
@@ -8,53 +8,8 @@ int main()
 {
 	auto input = utils::ReadInput( "input.txt" );
 
-	//for ( auto line : input )
-	//{
-	//	std::cout << line << "\n";
-	//}
-	//std::cout << "\n";
-
 	std::vector<bool> expandVert( input.size(), true );
 	std::vector<bool> expandHorz( input[0].size(), true );
-
-	for ( std::size_t y = 0; y < input.size(); y++ )
-	{
-		for ( std::size_t x = 0; x < input[y].size(); x++ )
-		{
-			if ( input[y][x] == '#' )
-			{
-				expandVert[y] = false;
-				expandHorz[x] = false;
-			}
-		}
-	}
-
-	for ( std::size_t i = 1; i <= expandVert.size(); i++ )
-	{
-		auto y = expandVert.size() - i;
-		if ( expandVert[y] )
-		{
-			input.insert( input.begin() + y, input[y] );
-		}
-	}
-
-	for ( std::size_t i = 1; i <= expandVert.size(); i++ )
-	{
-		auto x = expandHorz.size() - i;
-		if ( expandHorz[x] )
-		{
-			for ( std::string& line : input )
-			{
-				line.insert( x, 1, '.' );
-			}
-		}
-	}
-
-	//for ( auto line : input )
-	//{
-	//	std::cout << line << "\n";
-	//}
-	//std::cout << "\n";
 
 	std::vector<utils::Pos> galaxies;
 
@@ -64,23 +19,49 @@ int main()
 		{
 			if ( input[y][x] == '#' )
 			{
+				expandVert[y] = false;
+				expandHorz[x] = false;
 				galaxies.emplace_back( x, y );
 			}
 		}
 	}
 
-	std::uint32_t sum = 0;
+	auto getExpansion = [&]( const utils::Pos& lhs, const utils::Pos& rhs, bool part2 ) -> std::uint64_t
+		{
+			std::uint64_t expFactor = part2 ? 999999 : 1;
+			std::uint64_t expansion = 0;
+			for ( int x = std::min( lhs.X, rhs.X ) + 1; x < std::max( lhs.X, rhs.X ); x++ )
+			{
+				if ( expandHorz[x] )
+				{
+					expansion += expFactor;
+				}
+			}
+
+			for ( int y = std::min( lhs.Y, rhs.Y ) + 1; y < std::max( lhs.Y, rhs.Y ); y++ )
+			{
+				if ( expandVert[y] )
+				{
+					expansion += expFactor;
+				}
+			}
+
+			return expansion;
+		};
+
+	std::uint32_t sum1 = 0;
+	std::uint64_t sum2 = 0;
 	for ( std::size_t i = 0; i < galaxies.size() - 1; i++ )
 	{
 		for ( std::size_t j = i + 1; j < galaxies.size(); j++ )
 		{
-			auto dist = galaxies[i].GetManDistance( galaxies[j] );
-			sum += dist;
-			//std::cout << i + 1 << "->" << j + 1 << " " << dist << "\n";
+			sum1 += galaxies[i].GetManDistance( galaxies[j] ) + getExpansion(galaxies[i], galaxies[j], false);
+			sum2 += galaxies[i].GetManDistance( galaxies[j] ) + getExpansion(galaxies[i], galaxies[j], true);
 		}
 	}
 
-	std::cout << sum << "\n";
+	std::cout << sum1 << "\n";
+	std::cout << sum2 << "\n";
 
 	return 0;
 }

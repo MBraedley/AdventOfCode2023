c100ca239b04aafb3769f71aaa2cc83a3c2de210
Michael Braedley <michael.braedley@cartenav.com>
2023-12-12 10:53:27 -0400

    Part 2 optimizations

--- Day12/Day12.cpp
+++ Day12/Day12.cpp
@@ -4,114 +4,126 @@
 #include "Day12.h"
 #include "Utils.h"
 
-//enum class Spring
-//{
-//	Operational,
-//	Damaged,
-//	Unknown,
-//};
+#include <thread>
+#include <future>
 
-std::vector<std::string> GetPermutations(const std::string& springs)
+std::uint64_t GetMatchCount( const std::string& springs, const std::regex& re )
 {
-	std::vector<std::string> ret;
-	auto rPos = springs.find_first_of('?');
+	auto rPos = springs.find_first_of( '?' );
 
-	if (rPos == springs.npos)
+	if ( rPos == springs.npos )
 	{
-		ret.push_back(springs);
-		return ret;
+		if ( std::regex_match( springs, re ) )
+		{
+			return 1;
+		}
+		return 0;
 	}
 
+	std::uint64_t count = 0;
+
 	std::string springs1 = springs;
-	springs1.replace(rPos, 1, 1, '.');
-	//ret.push_back(springs1);
-	auto moreOptions = GetPermutations(springs1);
-	ret.insert(ret.end(), moreOptions.begin(), moreOptions.end());
+	springs1.replace( rPos, 1, 1, '.' );
+	if ( std::regex_match( springs1, re ) )
+	{
+		count += GetMatchCount( springs1, re );
+	}
 
 	std::string springs2 = springs;
-	springs2.replace(rPos, 1, 1, '#');
-	//ret.push_back(springs2);
-	moreOptions = GetPermutations(springs2);
-	ret.insert(ret.end(), moreOptions.begin(), moreOptions.end());
+	springs2.replace( rPos, 1, 1, '#' );
+	if ( std::regex_match( springs2, re ) )
+	{
+		count += GetMatchCount( springs2, re );
+	}
 
-	return ret;
+	return count;
 }
 
-std::regex GenerateRegex(const std::vector<int>& springGroups)
+std::regex GenerateRegex( const std::vector<int>& springGroups, int reps )
 {
 	bool beginningMatch = true;
 	std::stringstream sstrm;
-	for (int group : springGroups)
+	for ( int i = 0; i < reps; i++ )
 	{
-		sstrm << (beginningMatch ? "\\.*" : "\\.+") << "#{" << group << "}";
-		beginningMatch = false;
+		for ( int group : springGroups )
+		{
+			sstrm << (beginningMatch ? "[\\.\\?]*" : "[\\.\\?]+") << "[#\\?]{" << group << "}";
+			beginningMatch = false;
+		}
 	}
 
-	sstrm << "\\.*";
+	sstrm << "[\\.\\?]*";
 
-	return std::regex(sstrm.str());
+	return std::regex( sstrm.str() );
 }
 
 int main()
 {
-	auto input = utils::ReadInput("input.txt");
+	std::chrono::system_clock::time_point tp = std::chrono::system_clock::now();
+
+	auto input = utils::ReadInput( "input.txt" );
 
 	std::vector<std::pair<std::string, std::vector<int>>> records;
 
-	for (auto line : input)
+	for ( auto line : input )
 	{
-		std::stringstream sstrm(line);
+		std::stringstream sstrm( line );
 		std::string r;
 		sstrm >> r;
-		//std::vector<Spring> vr;
-		//for (char c : r)
-		//{
-		//	switch (c)
-		//	{
-		//	case '.':
-		//		vr.push_back(Spring::Operational);
-		//		break;
-		//	case '#':
-		//		vr.push_back(Spring::Damaged);
-		//		break;
-		//	case '?':
-		//		vr.push_back(Spring::Unknown);
-		//		break;
-		//	default:
-		//		throw std::invalid_argument("invalid spring type");
-		//		break;
-		//	}
-		//}
 
 		int count;
 		std::vector<int> check;
 		char c;
-		while (sstrm >> count)
+		while ( sstrm >> count )
 		{
-			check.push_back(count);
+			check.push_back( count );
 			sstrm >> c;
 		}
-		records.emplace_back(r, check);
+		records.emplace_back( r, check );
 	}
 
-	std::vector<std::uint32_t> matchCounts;
-	for (auto [record, groups] : records)
+	std::vector<std::future<std::size_t>> futures1;
+	std::vector<std::future<std::size_t>> futures2;
+
+	for ( auto& [record, groups] : records )
 	{
-		auto re = GenerateRegex(groups);
-
-		std::uint32_t count = 0;
-		for (auto r : GetPermutations(record))
-		{
-			if (std::regex_match(r, re))
+		futures1.emplace_back( std::async( std::launch::async, [&]() -> std::size_t
 			{
-				count++;
-			}
-		}
+				auto re = GenerateRegex( groups, 1 );
+				return GetMatchCount( record, re );
+			} ) );
 
-		matchCounts.push_back(count);
+		futures2.emplace_back( std::async( std::launch::async, [&]() -> std::size_t
+			{
+				auto re = GenerateRegex( groups, 5 );
+				std::stringstream expRecord;
+				expRecord << record;
+				for ( int i = 1; i < 5; i++ )
+				{
+					expRecord << "?" << record;
+				}
+				return GetMatchCount( expRecord.str(), re );
+			} ) );
 	}
 
-	std::cout << std::accumulate(matchCounts.begin(), matchCounts.end(), 0) << "\n";
+	std::size_t count1 = 0;
+	for ( auto& f : futures1 )
+	{
+		count1 += f.get();
+	}
+
+	std::cout << count1 << "\n";
+	std::cout << "Time: " << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - tp) << "\n";
+
+	//part 2
+	std::size_t count2 = 0;
+	for ( auto& f : futures2 )
+	{
+		count2 += f.get();
+	}
+
+	std::cout << count2 << "\n";
+	std::cout << "Time: " << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - tp) << "\n";
 
 	return 0;
 }

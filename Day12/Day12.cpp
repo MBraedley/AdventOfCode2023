ce71899932010633bc06b46c8f1206e95cc9bc23
Michael Braedley <mike.braedley@gmail.com>
2023-12-13 08:12:22 -0400

    part 2 progress

--- Day12/Day12.cpp
+++ Day12/Day12.cpp
@@ -6,8 +6,56 @@
 
 #include <thread>
 #include <future>
+#include <unordered_map>
 
-std::uint64_t GetMatchCount( const std::string& springs, const std::regex& re )
+using MemoKey = std::pair<std::string, int>;
+
+namespace std
+{
+	template<>
+	struct hash<MemoKey>
+	{
+		std::size_t operator()(const MemoKey& obj) const noexcept
+		{
+			auto h1 = std::hash<std::string>{}(obj.first);
+			auto h2 = std::hash<int>{}(obj.second);
+
+			return h1 ^ h2;
+		}
+	};
+}
+
+std::unordered_map<MemoKey, std::smatch> MemoPad;
+std::unordered_map<int, std::regex> RegexLookup;
+
+//const std::regex TrailingCheck(R"([^#]*)");
+
+std::regex& GetRegex(int group)
+{
+	if (!RegexLookup.contains(group))
+	{
+		std::stringstream sstrm;
+		sstrm << R"([^#]?([\?#]{)" << group << R"(})[^#])";
+		RegexLookup.emplace(group, sstrm.str());
+	}
+	return RegexLookup[group];
+}
+
+const std::smatch& DoRegex(std::string str, int group)
+{
+	MemoKey key = std::make_pair(str, group);
+	MemoKey keyCopy = key;
+	if (!MemoPad.contains(key))
+	{
+		std::smatch m;
+		std::regex& re = GetRegex(group);
+		std::regex_search(key.first, m, re);
+		MemoPad.emplace(std::move(key), m);
+	}
+	return MemoPad[keyCopy];
+}
+
+std::uint64_t GetMatchCount( const std::string& springs, const std::vector<int>& groups )
 {
 	auto rPos = springs.find_first_of( '?' );
 
@@ -17,47 +65,55 @@ std::uint64_t GetMatchCount( const std::string& springs, const std::regex& re )
 		return 1;
 	}
 
-	//if ( rPos == springs.npos )
-	//{
-	//	//We already know this string matches;
-	//	return 1;
-	//}
-
 	std::uint64_t count = 0;
 
 	std::string springs1 = springs;
 	springs1.replace( rPos, 1, 1, '.' );
-	if ( std::regex_match( springs1, re ) )
+	std::string unmatched = springs1;
+	bool passed = true;
+	for (int group : groups)
 	{
-		count += GetMatchCount( springs1, re );
+		auto m = DoRegex(unmatched, group);
+		if (m.empty())
+		{
+			passed = false;
+			break;
+		}
+		else
+		{
+			unmatched = m.suffix();
+		}
+	}
+
+	if (passed && unmatched.find('#') == unmatched.npos )
+	{
+		count += GetMatchCount(springs1, groups);
 	}
 
 	std::string springs2 = springs;
 	springs2.replace( rPos, 1, 1, '#' );
-	if ( std::regex_match( springs2, re ) )
+	unmatched = springs2;
+	passed = true;
+	for (int group : groups)
 	{
-		count += GetMatchCount( springs2, re );
-	}
-
-	return count;
-}
-
-std::regex GenerateRegex( const std::vector<int>& springGroups, int reps )
-{
-	bool beginningMatch = true;
-	std::stringstream sstrm;
-	for ( int i = 0; i < reps; i++ )
-	{
-		for ( int group : springGroups )
+		auto m = DoRegex(unmatched, group);
+		if (m.empty())
 		{
-			sstrm << (beginningMatch ? "[\\.\\?]*" : "[\\.\\?]+") << "[#\\?]{" << group << "}";
-			beginningMatch = false;
+			passed = false;
+			break;
+		}
+		else
+		{
+			unmatched = m.suffix();
 		}
 	}
 
-	sstrm << "[\\.\\?]*";
+	if (passed && unmatched.find('#') == unmatched.npos)
+	{
+		count += GetMatchCount(springs1, groups);
+	}
 
-	return std::regex( sstrm.str() );
+	return count;
 }
 
 int main()
@@ -73,6 +129,7 @@ int main()
 		std::stringstream sstrm( line );
 		std::string r;
 		sstrm >> r;
+		r += ".";//make the regex easier
 
 		int count;
 		std::vector<int> check;
@@ -87,40 +144,39 @@ int main()
 
 	std::vector<std::future<std::size_t>> futures1;
 	std::vector<std::future<std::size_t>> futures2;
+	std::size_t count1 = 0;
+	std::size_t count2 = 0;
 
 	for ( auto& [record, groups] : records )
 	{
-		futures1.emplace_back( std::async( std::launch::async, [&]() -> std::size_t
-			{
-				auto re = GenerateRegex( groups, 1 );
-				return GetMatchCount( record, re );
-			} ) );
+		count1 += GetMatchCount(record, groups);
+		//futures1.emplace_back( std::async( std::launch::async, [&]() -> std::size_t
+		//	{
+		//		return GetMatchCount( record, groups );
+		//	} ) );
 
-		futures2.emplace_back( std::async( std::launch::async, [&]() -> std::size_t
-			{
-				auto re = GenerateRegex( groups, 5 );
-				std::stringstream expRecord;
-				expRecord << record;
-				for ( int i = 1; i < 5; i++ )
-				{
-					expRecord << "?" << record;
-				}
-				return GetMatchCount( expRecord.str(), re );
-			} ) );
+		//futures2.emplace_back( std::async( std::launch::async, [&]() -> std::size_t
+		//	{
+		//		auto re = GenerateRegex( groups, 5 );
+		//		std::stringstream expRecord;
+		//		expRecord << record;
+		//		for ( int i = 1; i < 5; i++ )
+		//		{
+		//			expRecord << "?" << record;
+		//		}
+		//		return GetMatchCount( expRecord.str(), re );
+		//	} ) );
 	}
 
-	std::size_t count1 = 0;
-	std::size_t longestBranch = 0;
-	for ( auto& f : futures1 )
-	{
-		count1 += f.get();
-	}
+	//for ( auto& f : futures1 )
+	//{
+	//	count1 += f.get();
+	//}
 
 	std::cout << count1 << "\n";
 	std::cout << "Time: " << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - tp) << "\n";
 
 	//part 2
-	std::size_t count2 = 0;
 	for ( auto& f : futures2 )
 	{
 		count2 += f.get();

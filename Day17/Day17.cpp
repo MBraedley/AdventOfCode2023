e72f06ce6702d0b620e50e154f6628fe9047ff7a
Michael Braedley <mike.braedley@gmail.com>
2023-12-25 19:44:34 -0400

    Day 17 part 1

--- Day17/Day17.cpp
+++ Day17/Day17.cpp
@@ -17,7 +17,18 @@ struct Step
 		stepsTaken(_stepsTaken),
 		currentCost(_currentCost),
 		inputSize(std::make_pair(_input[0].size(), _input.size()))
-	{}
+	{
+		double totalCost = 0.0;
+		for (const auto& line : _input)
+		{
+			for (char c : line)
+			{
+				totalCost += c - '0';
+			}
+		}
+
+		averageSquare = totalCost / (inputSize.first * inputSize.second);
+	}
 
 	utils::Pos pos;
 	utils::Direction lastStep;
@@ -26,35 +37,47 @@ struct Step
 	std::set<utils::Pos> visitedPoints;
 
 	std::pair<std::size_t, std::size_t> inputSize;
+	double averageSquare;
 
 	double GetCost() const
 	{
-		double avgCost = static_cast<double>(currentCost) / static_cast<double>(pos.GetManDistance(utils::Pos()));
-		auto totalCost = currentCost + avgCost * ( pos.GetManDistance(utils::Pos(inputSize.first, inputSize.second)) + visitedPoints.size());
-		return totalCost;
+		double remainingCost = pos.GetManDistance(utils::Pos(inputSize.first - 1, inputSize.second - 1));
+		return currentCost + remainingCost;
 	}
 };
 
 auto operator<=>(const Step& lhs, const Step& rhs)
 {
-	//if (rhs.currentCost == lhs.currentCost)
-	//{
-	//	if (rhs.pos.GetManDistance(utils::Pos()) == lhs.pos.GetManDistance(utils::Pos()))
-	//	{
-	//		if (rhs.visitedPoints.size() == lhs.visitedPoints.size())
-	//		{
-	//			if (rhs.stepsTaken == lhs.stepsTaken)
-	//			{
-	//				return rhs.pos <=> lhs.pos;
-	//			}
-	//			return rhs.stepsTaken <=> lhs.stepsTaken;
-	//		}
-	//		return rhs.visitedPoints.size() <=> lhs.visitedPoints.size();
-	//	}
-	//	return rhs.pos.GetManDistance(utils::Pos()) <=> lhs.pos.GetManDistance(utils::Pos());
-	//}
-	//return rhs.currentCost <=> lhs.currentCost;
-	return rhs.GetCost() <=> lhs.GetCost();
+	if (lhs.currentCost == rhs.currentCost)
+	{
+		if (lhs.pos == rhs.pos)
+		{
+			if (lhs.lastStep == rhs.lastStep)
+			{
+				if (lhs.stepsTaken == rhs.stepsTaken)
+				{
+					return lhs.visitedPoints.size() <=> rhs.visitedPoints.size();
+				}
+
+				return lhs.stepsTaken <=> rhs.stepsTaken;
+			}
+
+			return lhs.lastStep <=> rhs.lastStep;
+		}
+
+		return lhs.pos <=> rhs.pos;
+	}
+
+	return lhs.currentCost <=> rhs.currentCost;
+}
+
+bool operator==(const Step& lhs, const Step& rhs)
+{
+	return lhs.pos == rhs.pos
+		&& lhs.currentCost == rhs.currentCost
+		&& lhs.lastStep == rhs.lastStep
+		&& lhs.stepsTaken == rhs.stepsTaken
+		&& lhs.visitedPoints == rhs.visitedPoints;
 }
 
 int main()
@@ -63,7 +86,13 @@ int main()
 
 	auto input = utils::ReadInput("input.txt");
 
-	std::priority_queue<Step> nextSteps;
+	struct compareCost
+	{
+		bool operator()(const Step& lhs, const Step& rhs) { return lhs.GetCost() > rhs.GetCost(); }
+	};
+
+	std::priority_queue<Step, std::vector<Step>, compareCost> nextSteps;
+	std::map<utils::Pos, std::set<Step>> posCosts;
 
 	Step start(utils::Pos(), utils::Direction::East, 0, 0, input);
 
@@ -71,29 +100,71 @@ int main()
 
 	utils::Pos end(input[0].size() - 1, input.size() - 1);
 
-	while (nextSteps.top().pos != end)
+	std::uint32_t lowestCost = std::numeric_limits<std::uint32_t>::max();
+
+	while (!nextSteps.empty())
 	{
 		auto p = nextSteps.top();
 
 		nextSteps.pop();
 
-		for (auto dir : { utils::Direction::North, utils::Direction::East, utils::Direction::South, utils::Direction::West })
-		{
-			auto np = p.pos.GetNeighbour(dir, input);
-			if (np.has_value() &&
-				!p.visitedPoints.contains(*np) &&
-				(dir != p.lastStep || p.stepsTaken < 3))
-			{
-				Step ns(*np, dir, dir == p.lastStep ? p.stepsTaken + 1 : 1, p.currentCost + (input[np->Y][np->X] - '0'), input);
-				ns.visitedPoints = p.visitedPoints;
-				ns.visitedPoints.emplace(p.pos);
+		bool validPath = true;
 
-				nextSteps.push(ns);
+		if (p.pos == end)
+		{
+			if (p.currentCost < lowestCost)
+			{
+				utils::PrintResult(p.currentCost, startTime);
+				lowestCost = p.currentCost;
+			}
+			validPath = false;
+		}
+		else if (posCosts.contains(p.pos))
+		{
+			if (posCosts[p.pos].contains(p))
+			{
+				validPath = false;
+			}
+			else
+			{
+				for (auto s : posCosts[p.pos])
+				{
+					if (p.currentCost >= s.currentCost)
+					{
+						if (p.stepsTaken == 3)
+						{
+							validPath = false;
+						}
+						else if (p.lastStep == s.lastStep && p.stepsTaken >= s.stepsTaken)
+						{
+							validPath = false;
+						}
+					}
+				}
+			}
+		}
+
+		if (validPath)
+		{
+			posCosts[p.pos].emplace(p);
+			for (auto dir : { utils::Direction::North, utils::Direction::East, utils::Direction::South, utils::Direction::West })
+			{
+				auto np = p.pos.GetNeighbour(dir, input);
+				if (np.has_value() &&
+					!p.visitedPoints.contains(*np) &&
+					(dir != p.lastStep || p.stepsTaken < 3))
+				{
+					Step ns(*np, dir, dir == p.lastStep ? p.stepsTaken + 1 : 1, p.currentCost + (input[np->Y][np->X] - '0'), input);
+					ns.visitedPoints = p.visitedPoints;
+					ns.visitedPoints.emplace(p.pos);
+
+					nextSteps.push(ns);
+				}
 			}
 		}
 	}
 
-	utils::PrintResult(nextSteps.top().currentCost + (input[end.Y][end.X] - '0'), startTime);
+	utils::PrintResult(lowestCost, startTime);
 
 	return 0;
 }
